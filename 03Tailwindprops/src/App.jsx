22f00b2617185ff8f298e93e8c2060c145144ce5
Sunny Jaiswal <jaiswalsunny055@gmail.com>
2024-04-23 23:57:05 +0530

    Tailwind and Props in react

--- 03Tailwindprops/src/App.jsx
+++ 03Tailwindprops/src/App.jsx
@@ -5,15 +5,19 @@ import './App.css'
 import Card from './Components/card'
 
 
-
 function App() {
   const [count, setCount] = useState(0)
+  let myObj ={
+    username : "Sunny",
+    age: 21
+  }
+  let newArr = [1,2,3,4];
 
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
    
-    <Card channel = "Chaiaurcode"/>
+    <Card channel = "Chaiaurcode" someobj= {newArr}/>
     <Card/>
 
     
