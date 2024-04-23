e66b62d7a0b02dd7ca8181a7caf0fd9463637846
Sunny Jaiswal <jaiswalsunny055@gmail.com>
2024-04-24 01:42:59 +0530

    Tailwind Components Done

--- 03Tailwindprops/src/App.jsx
+++ 03Tailwindprops/src/App.jsx
@@ -17,8 +17,10 @@ function App() {
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
    
-    <Card channel = "Chaiaurcode" someobj= {newArr}/>
-    <Card/>
+    {/* <Card channel = "Chaiaurcode" someobj= {newArr}/> */}
+    <Card username ="Chairucode" btnText ="Click Me" />
+    <Card username= "jaiswal" btnText="Visit Me"/>
+
 
     
     </>
