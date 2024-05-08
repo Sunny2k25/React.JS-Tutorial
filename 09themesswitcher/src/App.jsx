faa76bb339bd72d681568acdc14a78e36f070037
Sunny Jaiswal <jaiswalsunny055@gmail.com>
2024-05-08 12:09:49 +0530

    Commit

--- 09themesswitcher/src/App.jsx
+++ 09themesswitcher/src/App.jsx
@@ -1,46 +1,43 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
-import { ThemeProvider } from './Contexts/theme'
-import ThemeBtn from './Components/ThemeBtn'
-import Card from './Components/Card'
+import "./App.css";
+import { ThemeProvider } from "./Contexts/theme";
+import ThemeBtn from "./Components/ThemeBtn";
+import Card from "./Components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [themeMode, setThemeMode] = useState("light")
+  const [count, setCount] = useState(0);
+  const [themeMode, setThemeMode] = useState("light");
 
-    const lightTheme = () =>{
-      setThemeMode("light")
-    }
-    const darkTheme = () =>{
-      setThemeMode("dark")
-    }
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
 
-    //actual change in theme
-    useEffect (()=>{
-      document.querySelector('html').classList.remove("light","dark")
-      document.querySelector('html').classList.add(themeMode) 
-    },[themeMode])
+  //actual change in theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
-   
   return (
-    
-    <ThemeProvider value ={{themeMode,lightTheme,darkTheme}}>
-    <div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/**Themebtn */}
-                        <ThemeBtn/>
-                    </div>
-
-                    <div className="w-full max-w-sm mx-auto">
-                       {/**Card */}
-                       <Card/>
-                    </div>
-                </div>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className = "flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/**Themebtn */}
+            <ThemeBtn />
+          </div>
+            <div className="w-full max-w-sm mx-auto">
+            {/**Card */}
+            <Card />
+          </div>
         </div>
-        </ThemeProvider>
-  )
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
