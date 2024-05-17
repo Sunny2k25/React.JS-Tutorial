ebfc33e369a5f5bfa4dc7f6e18708b4cf2c1c160
Sunny Jaiswal <jaiswalsunny055@gmail.com>
2024-05-17 16:36:08 +0530

    Update Todo Project

--- 10todoContextLocal/src/App.jsx
+++ 10todoContextLocal/src/App.jsx
@@ -1,21 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { TodoProvider,addTodo} from "./contexts/TodoContext";
+import { TodoProvider} from "./contexts/TodoContext";
 
 function App() {
-  const [todos, setTodos] = useState ([])
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]); //isme apan ne purani value bhi daal di or nayi bhi
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+
+    {
+      /**prev.map((eachVal) => {
+      if(eachVal.id===id){
+        todo;
+      }
+      else{
+        prevTodo;
+      }
+    })**/
+    }
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id != id));
+  };
+
+  const toggleCompleted = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length >0)
+      {
+        setTodos(todos)
+      }
+  },[])
 
 
-  const addTodo = () =>{
-
-  }
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
 
   return (
-      <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo, toggleCompleted}}>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleCompleted }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your TodosX
+            Manage Your Todos
           </h1>
           <div className="mb-4">{/* Todo form goes here */}</div>
           <div className="flex flex-wrap gap-y-3">
