0fa2cafef76fdf7dff1fad197f011004debbc03b
Sunny Jaiswal <jaiswalsunny055@gmail.com>
2025-02-07 02:02:46 +0530

    update

--- 11practiceReact/src/App.jsx
+++ 11practiceReact/src/App.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <div className="container bg-red-500">
+      <div className="container bg-blue-500">
         <h1>Hello World</h1>
         
       </div>
-      <button className="">Hey</button>
+      <button className="bg-lime-200">Hey</button>
     </div>
   )
 }
